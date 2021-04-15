ebd700c3380b0a48e95795793eeb1d4278c92783
SheliaC <vornadous@gmail.com>
2021-04-15 11:33:04 -0400

    Header and Footer fixed

--- nucampsite/src/components/MainComponent.js
+++ nucampsite/src/components/MainComponent.js
@@ -21,9 +21,10 @@ class Main extends Component {
     render() {
         return (
             <div>
-                
+                <Header />
                 <Directory campsites={this.state.campsites} onClick={campsiteId => this.onCampsiteSelect(campsiteId)}/>
                 <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]} />
+                <Footer />
             </div>
         );
     };
